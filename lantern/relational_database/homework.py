e14f19773ca0a391c97e06b5f63e61b051bce212
winston <wnatakillemall@gmail.com>
2020-04-01 23:20:27 +0300

    added hw

--- lantern/relational_database/homework.py
+++ lantern/relational_database/homework.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import json
 
 def task_1_add_new_record_to_db(con) -> None:
     """
@@ -19,7 +19,33 @@ def task_1_add_new_record_to_db(con) -> None:
     Returns: 92 records
 
     """
-    pass
+    dict = {
+        'customer_name': 'Thomas',
+        'contactname': 'David',
+        'address': 'Some Address',
+        'city': 'London',
+        'postalcode': '774',
+        'country': 'Singapore',
+    }
+
+    try:
+        con.autocommit = True
+        cursor = con.cursor()
+        sql = f"""
+        INSERT INTO customers(customername, contactname, address, city, postalcode, country) 
+        VALUES('{dict.get('customer_name')}', 
+                '{dict.get('contactname')}',
+                '{dict.get('address')}',
+                '{dict.get('city')}',
+                '{dict.get('postalcode')}',
+                '{dict.get('country')}'
+                );
+        """
+        cursor.execute(sql)
+        con.commit()
+    except:
+        raise Exception
+
 
 
 def task_2_list_all_customers(cur) -> list:
@@ -32,7 +58,17 @@ def task_2_list_all_customers(cur) -> list:
     Returns: 91 records
 
     """
-    pass
+    try:
+        cursor = cur
+        sql = """
+        select * from customers;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_3_list_customers_in_germany(cur) -> list:
@@ -44,7 +80,18 @@ def task_3_list_customers_in_germany(cur) -> list:
 
     Returns: 11 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = """
+        select * from customers
+        where country = 'Germany';
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_4_update_customer(con):
@@ -56,7 +103,24 @@ def task_4_update_customer(con):
     Returns: 91 records with updated customer
 
     """
-    pass
+
+    try:
+        con.autocommit = True
+        cursor = con.cursor()
+        sql = f"""
+        update customers
+        set customername = 'Johnny Depp'
+        where customername in (select customername from customers limit 1);
+        """
+        cursor.execute(sql)
+        con.commit()
+        sql = f"""select * from customers"""
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        con.commit()
+    except:
+        raise Exception
+    return records
 
 
 def task_5_delete_the_last_customer(con) -> None:
@@ -66,7 +130,17 @@ def task_5_delete_the_last_customer(con) -> None:
     Args:
         con: psycopg connection
     """
-    pass
+    try:
+        con.autocommit = True
+        cursor = con.cursor()
+        sql = f"""
+        DELETE FROM customers
+        WHERE CustomerID in (select CustomerID from customers order by CustomerID desc limit 1);
+        """
+        cursor.execute(sql)
+        con.commit()
+    except:
+        raise Exception
 
 
 def task_6_list_all_supplier_countries(cur) -> list:
@@ -79,7 +153,17 @@ def task_6_list_all_supplier_countries(cur) -> list:
     Returns: 29 records
 
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select country from suppliers;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_7_list_supplier_countries_in_desc_order(cur) -> list:
@@ -92,7 +176,17 @@ def task_7_list_supplier_countries_in_desc_order(cur) -> list:
     Returns: 29 records in descending order
 
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select country from suppliers order by country desc;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_8_count_customers_by_city(cur):
@@ -105,7 +199,21 @@ def task_8_count_customers_by_city(cur):
     Returns: 69 records in descending order
 
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select city
+            ,count(city) AS "qty" 
+        from customers
+        group by city
+        order by "qty" desc;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_9_count_customers_by_country_with_than_10_customers(cur):
@@ -117,7 +225,21 @@ def task_9_count_customers_by_country_with_than_10_customers(cur):
 
     Returns: 3 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select count(country) AS "count" 
+            ,country
+        from customers
+        group by country
+        having count(country) > 10;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_10_list_first_10_customers(cur):
@@ -126,7 +248,19 @@ def task_10_list_first_10_customers(cur):
 
     Results: 10 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select *
+        from customers
+        limit 10;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_11_list_customers_starting_from_11th(cur):
@@ -138,7 +272,21 @@ def task_11_list_customers_starting_from_11th(cur):
 
     Returns: 11 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select *
+        from customers
+        Offset 11 Rows;
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        # j_ar = json.dumps(records)
+        # print(j_ar)
+    except:
+        raise Exception
+
+    return records
 
 
 def task_12_list_suppliers_from_specified_countries(cur):
@@ -150,7 +298,19 @@ def task_12_list_suppliers_from_specified_countries(cur):
 
     Returns: 8 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+        select supplierid, suppliername, contactname, city, country
+        from suppliers
+        where country in ('USA', 'UK', 'Japan');
+        """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_13_list_products_from_sweden_suppliers(cur):
@@ -162,7 +322,20 @@ def task_13_list_products_from_sweden_suppliers(cur):
 
     Returns: 3 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+           select p.productname
+           from products p
+                join suppliers s on s.supplierid = p.supplierid
+           where s.country='Sweden';
+           """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_14_list_products_with_supplier_information(cur):
@@ -174,7 +347,25 @@ def task_14_list_products_with_supplier_information(cur):
 
     Returns: 77 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+           select p.productid,
+                p.productname,
+                p.unit,
+                p.price,
+                s.country,
+                s.city,
+                s.suppliername
+           from products p
+                join suppliers s on s.supplierid = p.supplierid;
+           """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_15_list_customers_with_any_order_or_not(cur):
@@ -186,7 +377,22 @@ def task_15_list_customers_with_any_order_or_not(cur):
 
     Returns: 213 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+           select c.customername,
+            c.contactname,
+            c.country,
+            o.orderid
+        from orders o
+            join customers c on c.CustomerID=o.CustomerID
+           """
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    except:
+        raise Exception
+
+    return records
 
 
 def task_16_match_all_customers_and_suppliers_by_country(cur):
@@ -198,4 +404,23 @@ def task_16_match_all_customers_and_suppliers_by_country(cur):
 
     Returns: 194 records
     """
-    pass
+    try:
+        cursor = cur
+        sql = f"""
+           select c.customername,
+                c.address,
+                c.country as "customercountry",
+                s.country as "suppliercountry",
+                s.supplierName
+        from customers c 
+            full join suppliers s on c.country=s.country
+            order by c.country, s.country;
+           """
+
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        len(records)
+    except:
+        raise Exception
+
+    return records

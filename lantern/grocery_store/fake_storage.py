e3dfd2a1c4dd94fefa8a8e65c38858bdab3215af
winston <wnatakillemall@gmail.com>
2020-04-05 14:21:50 +0300

    groc_store

---
+++ lantern/grocery_store/fake_storage.py
@@ -0,0 +1,34 @@
+from itertools import count
+from store_app import NoSuchUserError
+
+
+class FakeStorage:
+    def __init__(self):
+        self._users = FakeUsers()
+
+    @property
+    def users(self):
+        return self._users
+
+
+class FakeUsers:
+    def __init__(self):
+        self._users = {}
+        self._id_counter = count(1)
+
+    def add(self, user):
+        user_id = next(self._id_counter)
+        self._users[user_id] = user
+        return user_id
+
+    def get_user_by_id(self, user_id):
+        try:
+            return self._users[user_id]
+        except KeyError:
+            raise NoSuchUserError(user_id)
+
+    def update_user_by_id(self, user_id, user):
+        if user_id in self._users:
+            self._users[user_id] = user
+        else:
+            raise NoSuchUserError(user_id)

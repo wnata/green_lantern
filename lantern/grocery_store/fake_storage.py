6000542c59a2185fbdddeec5d25a31508a7da56b
Illia Sukonnik <illia.sukonnik@made.com>
2020-04-05 11:00:16 +0100

    get user

--- lantern/grocery_store/fake_storage.py
+++ lantern/grocery_store/fake_storage.py
@@ -1,4 +1,6 @@
 from itertools import count
+from store_app import NoSuchUserError
+
 
 class FakeStorage:
     def __init__(self):
@@ -11,9 +13,15 @@ class FakeStorage:
 class FakeUsers:
     def __init__(self):
         self._users = {}
-        self._id_counter = count()
+        self._id_counter = count(1)
 
     def add(self, user):
         user_id = next(self._id_counter)
         self._users[user_id] = user
         return user_id
+
+    def get_user_by_id(self, user_id):
+        try:
+            return self._users[user_id]
+        except KeyError:
+            raise NoSuchUserError(user_id)
--- lantern/grocery_store/test_store.py
+++ lantern/grocery_store/test_store.py
@@ -24,5 +24,26 @@ class TestUsers(Initializer):
             '/users',
             json={'name':'John Doe'}
         )
-        assert resp.status_code == 200
+        assert resp.status_code == 201
         assert resp.json == {'user_id': 1}
+
+        resp = self.client.post(
+            '/users',
+            json={'name':'Andrew Derkach'}
+        )
+        assert resp.json == {'user_id': 2}
+
+    def test_successful_get_user(self):
+        resp = self.client.post(
+            '/users',
+            json={'name':'John Doe'}
+        )
+        user_id = resp.json['user_id']
+        resp = self.client.get(f'/users/{user_id}')
+        assert resp.status_code == 200
+        assert resp.json == {'name':'John Doe'}
+
+    def test_get_unexistent_user(self):
+        resp = self.client.get(f'/users/1')
+        assert resp.status_code == 404
+        assert resp.json == {'error':'No such user_id 1'}

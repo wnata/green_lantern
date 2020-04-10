23bca09dedcd252c0928c54c1ea9bcba887a67e1
dvasilov <dvasilov@echoua.com>
2020-04-10 18:17:00 +0300

    Refactor material structure

---
+++ lantern/flask/flask_intro/practice/grocery_store/test_store.py
@@ -0,0 +1,49 @@
+import inject
+
+from store_app import app
+from fake_storage import FakeStorage
+
+
+def configure_test(binder):
+    db = FakeStorage()
+    binder.bind('DB', db)
+
+
+class Initializer:
+    def setup(self):
+        inject.clear_and_configure(configure_test)
+
+        app.config['TESTING'] = True
+        with app.test_client() as client:
+            self.client = client
+
+
+class TestUsers(Initializer):
+    def test_create_new(self):
+        resp = self.client.post(
+            '/users',
+            json={'name':'John Doe'}
+        )
+        assert resp.status_code == 201
+        assert resp.json == {'user_id': 1}
+
+        resp = self.client.post(
+            '/users',
+            json={'name':'Andrew Derkach'}
+        )
+        assert resp.json == {'user_id': 2}
+
+    def test_successful_get_user(self):
+        resp = self.client.post(
+            '/users',
+            json={'name':'John Doe'}
+        )
+        user_id = resp.json['user_id']
+        resp = self.client.get(f'/users/{user_id}')
+        assert resp.status_code == 200
+        assert resp.json == {'name':'John Doe'}
+
+    def test_get_unexistent_user(self):
+        resp = self.client.get(f'/users/1')
+        assert resp.status_code == 404
+        assert resp.json == {'error':'No such user_id 1'}

b6479373b5530fea305381d3ffd0cdbcf27752f2
Vooply <pv.yaschuk@gmail.com>
2020-04-26 13:53:30 +0300

    user written to data_base

--- lantern/flask/flask_SQLalchemy/practice/app.py
+++ lantern/flask/flask_SQLalchemy/practice/app.py
@@ -3,9 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, drop_database, database_exists
 
 from config import Config
+from populate_data import get_users
 
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
@@ -18,3 +30,10 @@ with app.app_context():
         print(f"Database does not exists {db.engine.url}")
         create_database(db.engine.url)
         print('Data base created')
+
+with app.app_context():
+    users = get_users()
+    for user in users:
+        db.session.add(User(**user))
+    db.session.commit()
+    print('Data written in data_base succesfuly')

5cd3d4753fb7e50f1721fd9ce679c432a2622b62
Andrew.py <61803449+andrsj@users.noreply.github.com>
2020-05-13 20:34:15 +0300

    Flask first migrate (#61)
    * Add CLI
    
    * First Migrate
    
    * FIX!! app.py

--- lantern/flask/flask_migrations/practice/grocery_store/app.py
+++ lantern/flask/flask_migrations/practice/grocery_store/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import MigrateCommand
 from flask_script import Server, Manager
 
 from grocery_store.config import Config
@@ -24,4 +24,5 @@ def make_db(app):
 def make_manager(app):
     manager = Manager(app)
     manager.add_command('runserver', Server(host=Config.HOST, port=Config.PORT))
+    manager.add_command('db', MigrateCommand)
     return manager

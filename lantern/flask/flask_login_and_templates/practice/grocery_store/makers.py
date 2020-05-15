629067c07494fbbc91503d1156f3f2d19bdd360c
Galyopa <galyopa@gmail.com>
2020-05-15 20:14:00 +0300

    Wrote make login manager

--- lantern/flask/flask_login_and_templates/practice/grocery_store/makers.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/makers.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_migrate import MigrateCommand
 from flask_script import Server, Manager
+from flask_login import LoginManager
 
 from grocery_store.config import Config
+from grocery_store.models import User
 from grocery_store.routes import users, goods, stores, auth, main
 from grocery_store.database import db
 from grocery_store.commands import Populate
@@ -30,3 +32,15 @@ def make_manager(app):
     manager.add_command('db', MigrateCommand)
     manager.add_command('populate', Populate)
     return manager
+
+
+def make_login_manager(app):
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    return login_manager

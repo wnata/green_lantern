fa5c77b6ad766ff3789dd608a974d68046653816
Wnata <wnatakillemall@gmail.com>
2020-05-22 16:55:29 +0300

    redisigned orders page

--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
@@ -3,6 +3,8 @@ from grocery_store.database import db
 from grocery_store.models import User, Store, Good, Order, OrderLine
 from flask_login import current_user, login_required
 
+from datetime import datetime
+
 main = Blueprint('main', __name__)
 
 
@@ -20,23 +22,31 @@ def profile():
 @main.route('/orders')
 @login_required
 def orders():
-    orders = Order.query.filter(Order.user_id==current_user.user_id).all()
-    orders_list = [] # takes {id:1, body:{store:name, goods:[1,2,3]}}
+    orders = Order.query.filter(Order.user_id == current_user.user_id).all()
+    orders_list = []  # takes {id:1, body:{store:name, goods:{name:'',price:''}}}
     for order in orders:
         order_dict = {}
 
         body_dict = {}
-        store = Store.query.filter(Store.store_id==order.store_id).first()
-        body_dict['store_name']= store.name
+        store = Store.query.filter(Store.store_id == order.store_id).first()
+        body_dict['store_name'] = store.name
 
         order_lines = OrderLine.query.filter(order.order_id == OrderLine.order_id).all()
-        goods = []
+
+        name_price = []
+        # [ {name:qwedfw}, {price:43} ]
+        order_sum = 0
         for order_line in order_lines:
-            good = Good.query.filter(Good.good_id==order_line.good_id).first()
-            goods.append(good.name)
+            goods = {}
+            good = Good.query.filter(Good.good_id == order_line.good_id).first()
+            goods[good.name] = good.price
+            order_sum += good.price
+            name_price.append(goods)
 
-        body_dict['goods'] = goods
 
+        body_dict['goods'] = name_price
+        body_dict['order_sum'] = order_sum
+        body_dict['Order_date'] = datetime.strftime(order.created_time,'%b %d, %Y')
         order_dict[order.order_id] = body_dict
         orders_list.append(order_dict)
 
